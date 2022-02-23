14007b3078b58369953e0c9ea5f4c66c08924102
Mike Slinn <mslinn@mslinn.com>
2022-02-22 22:51:11 -0500

    -

---
+++ test/test2.rb
@@ -0,0 +1,25 @@
+require 'slim'
+
+# Simple class to represent an environment
+class Env
+  attr_accessor :name
+end
+
+scope = Env.new
+scope.name = "test this layout"
+
+layout =<<EOS
+h1 Hello
+.content
+  = yield
+EOS
+
+contents =<<EOS
+  = name
+EOS
+
+layout = Slim::Template.new { layout }
+content = Slim::Template.new { contents }.render(scope)
+
+puts layout.render{ content }
+
