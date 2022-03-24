ae5084a1e25175391d88c07a2bf5ab721cb60c71
Mike Slinn <mslinn@micronauticsresearch.com>
2022-03-24 14:52:07 -0400

    -

--- jekyll_bootstrap5_tabs.gemspec
+++ jekyll_bootstrap5_tabs.gemspec
@@ -4,7 +4,7 @@ require_relative "lib/jekyll_bootstrap5_tabs/version"
 
 # rubocop:disable Metrics/BlockLength
 Gem::Specification.new do |spec|
-  github_page = "https://github.com/mslinn/jekyll_bootstrap5_tabs"
+  github = "https://github.com/mslinn/jekyll_bootstrap5_tabs"
 
   spec.authors = ["Artur Gabitov", "Mike Slinn"]
   spec.bindir = "bin"
@@ -13,44 +13,35 @@ Gem::Specification.new do |spec|
     Two new Liquid tags are provided: tabs and tab.
   END_DESC
   spec.email = ["applifort@gmail.com", "mslinn@mslinn.com"]
-
-  # Specify which files should be added to the gem when it is released.
-  # The `git ls-files -z` loads the files in the RubyGem that have been added into git.
-  spec.files = Dir.chdir(File.expand_path(__dir__)) do
-    `git ls-files -z`.split("\x0").reject do |f|
-      (f == __FILE__) || f.match(%r!\A(?:(?:bin|test|spec|features)/|\.(?:git|travis|circleci)|appveyor)!)
-    end
-  end
-
+  spec.files = Dir[".rubocop.yml", "LICENSE.*", "Rakefile", "{lib,spec}/**/*", "*.gemspec", "*.md"]
   spec.homepage = "https://mslinn.com/blog/2022/02/13/jekyll-gem.html"
   spec.license = "MIT"
   spec.metadata = {
     "allowed_push_host" => "https://rubygems.org",
-    "bug_tracker_uri"   => "#{github_page}/issues",
-    "changelog_uri"     => "#{github_page}/CHANGELOG.md",
+    "bug_tracker_uri"   => "#{github}/issues",
+    "changelog_uri"     => "#{github}/CHANGELOG.md",
     "homepage_uri"      => spec.homepage,
-    "source_code_uri"   => github_page,
+    "source_code_uri"   => github,
   }
   spec.name = "jekyll_bootstrap5_tabs"
+  spec.post_install_message = <<~END_MESSAGE
+
+    Thanks for installing #{spec.name}!
+
+  END_MESSAGE
   spec.require_paths = ["lib"]
   spec.required_ruby_version = ">= 2.6.0"
   spec.summary = "Jekyll plugin that enables Bootstrap 5 tabs in Jekyll 4 websites."
   spec.version = JekyllBootstrap5Tabs::VERSION
 
-  spec.executables = spec.files.grep(%r!\Aexe/!) { |f| File.basename(f) }
-
   spec.add_dependency "jekyll", ">= 3.5.0"
   spec.add_dependency "jekyll_plugin_logger"
   spec.add_dependency "slim", "~> 3.0"
 
-  spec.add_development_dependency "bundler"
   spec.add_development_dependency "debase"
-  spec.add_development_dependency "rake"
-  spec.add_development_dependency "rspec", "~> 3.0"
-  spec.add_development_dependency "rubocop"
-  spec.add_development_dependency "rubocop-jekyll"
-  spec.add_development_dependency "rubocop-rake"
-  spec.add_development_dependency "rubocop-rspec"
+  # spec.add_development_dependency "rubocop-jekyll"
+  # spec.add_development_dependency "rubocop-rake"
+  # spec.add_development_dependency "rubocop-rspec"
   spec.add_development_dependency "ruby-debug-ide"
 end
 # rubocop:enable Metrics/BlockLength
--- lib/jekyll_bootstrap5_tabs.rb
+++ lib/jekyll_bootstrap5_tabs.rb
@@ -11,93 +11,91 @@ module JekyllBootstrap5Name
   PLUGIN_NAME = "jekyll_bootstrap5_tabs"
 end
 
-module Jekyll
-  # Handles the outer {% tabs %}{% endtabs %} Liquid block for Bootstrap 5
-  class TabsBlock < Liquid::Block
-    def initialize(tag, args, _)
-      super
+# Handles the outer {% tabs %}{% endtabs %} Liquid block for Bootstrap 5
+class TabsBlock < Liquid::Block
+  def initialize(tag, args, _)
+    super
 
-      raise SyntaxError, "#{tag} requires name" if args.empty?
+    raise SyntaxError, "#{tag} requires name" if args.empty?
 
-      @logger = PluginLogger.new(self)
+    @logger = PluginMetaLogger.new_logger(self)
 
-      argv = args.strip.split
-      @tab_name = argv[0] # TODO @tab_name is never used. Should act as a namespace.
+    argv = args.strip.split
+    @tab_name = argv[0] # TODO @tab_name is never used. Should act as a namespace.
 
-      # Usage can override default and enable pretty-printing, not possible to disable per-tab
-      @pretty_print = false
-      if argv.length > 1 && argv[1].casecmp('pretty').zero?
-        @pretty_print = true
-        @logger.info { "Bootstrap tab pretty-printing is enabled for #{@tab_name}" }
-      end
-    end
-
-    # @param config [YAML] Configuration data that might contain a entry for `jekyll_bootstrap5_tabs`
-    # @param progname [String] The name of the `option:` subentry to look for underneath the `jekyll_bootstrap5_tabs` entry
-    # @return [TrueClass, FalseClass]
-    def check_config_boolean(config, _option)
-      tabs_options = config['jekyll_bootstrap5_tabs']
-      return false if tabs_options.nil?
-
-      hash = tabs_options.detect { |opt| opt["pretty"] }
-      @logger.debug { "tabs_options = #{tabs_options}" }
-      @logger.debug { "hash = #{hash}" }
-      !hash.nil? && hash['pretty']
-    end
-
-    def template_path(template_name)
-      dir = File.dirname(__FILE__)
-      File.join(dir, template_name.to_s)
-    end
-
-    def render(context)
-      site = context.registers[:site]
-      # Set the pretty-print option for the Slim engine
-      # Global configuration provides the default value of @pretty_print
-      if check_config_boolean(site.config, 'pretty')
-        @pretty_print = true
-        @logger.info { "Bootstrap tab pretty-printing is enabled by default for the entire Jekyll site." }
-      end
-
-      @environment = context.environments.first  # Has type Jekyll::Drops::UnifiedPayloadDrop
-      @logger.debug { "TabsBlock.render: @environment = '#{@environment}'" }
-      super
-
-      template_file_path = template_path(DEFAULT_TEMPLATE)
-      Slim::Engine.set_options :pretty => @pretty_print
-      template = Slim::Template.new(template_file_path)
-      template.render(self)
+    # Usage can override default and enable pretty-printing, not possible to disable per-tab
+    @pretty_print = false
+    if argv.length > 1 && argv[1].casecmp('pretty').zero?
+      @pretty_print = true
+      @logger.info { "Bootstrap tab pretty-printing is enabled for #{@tab_name}" }
     end
   end
 
-  # Handles the inner {% tab %}{% endtab %} Liquid block for Bootstrap 5
-  class TabBlock < Liquid::Block
-    def initialize(tag, args, _)
-      super
+  # @param config [YAML] Configuration data that might contain a entry for `jekyll_bootstrap5_tabs`
+  # @param progname [String] The name of the `option:` subentry to look for underneath the `jekyll_bootstrap5_tabs` entry
+  # @return [TrueClass, FalseClass]
+  def check_config_boolean(config, _option)
+    tabs_options = config['jekyll_bootstrap5_tabs']
+    return false if tabs_options.nil?
 
-      @logger = PluginLogger.new(self)
+    hash = tabs_options.detect { |opt| opt["pretty"] }
+    @logger.debug { "tabs_options = #{tabs_options}" }
+    @logger.debug { "hash = #{hash}" }
+    !hash.nil? && hash['pretty']
+  end
 
-      @tabs_group, @tab = split_params(args.strip)
-      @logger.debug { "TabBlock: @tabs_group = '#{@tabs_group}', @tab = '#{@tab}'" }
-      raise SyntaxError, "Block #{tag} requires tabs name" if @tabs_group.empty? || @tab.empty?
+  def template_path(template_name)
+    dir = File.dirname(__FILE__)
+    File.join(dir, template_name.to_s)
+  end
+
+  def render(context)
+    site = context.registers[:site]
+    # Set the pretty-print option for the Slim engine
+    # Global configuration provides the default value of @pretty_print
+    if check_config_boolean(site.config, 'pretty')
+      @pretty_print = true
+      @logger.info { "Bootstrap tab pretty-printing is enabled by default for the entire Jekyll site." }
     end
 
-    def render(context)
-      content = super
+    @environment = context.environments.first  # Has type Jekyll::Drops::UnifiedPayloadDrop
+    @logger.debug { "TabsBlock.render: @environment = '#{@environment}'" }
+    super
 
-      environment = context.environments.first # Has type Jekyll::Drops::UnifiedPayloadDrop
-      environment["tabs-#{@tabs_group}"] ||= {}
-      environment["tabs-#{@tabs_group}"][@tab] = content
-    end
+    template_file_path = template_path(DEFAULT_TEMPLATE)
+    Slim::Engine.set_options :pretty => @pretty_print
+    template = Slim::Template.new(template_file_path)
+    template.render(self)
+  end
+end
 
-    private
+# Handles the inner {% tab %}{% endtab %} Liquid block for Bootstrap 5
+class TabBlock < Liquid::Block
+  def initialize(tag, args, _)
+    super
 
-    def split_params(params)
-      params.split('#')
-    end
+    @logger = PluginMetaLogger.new_logger(self)
+
+    @tabs_group, @tab = split_params(args.strip)
+    @logger.debug { "TabBlock: @tabs_group = '#{@tabs_group}', @tab = '#{@tab}'" }
+    raise SyntaxError, "Block #{tag} requires tabs name" if @tabs_group.empty? || @tab.empty?
+  end
+
+  def render(context)
+    content = super
+
+    environment = context.environments.first # Has type Jekyll::Drops::UnifiedPayloadDrop
+    environment["tabs-#{@tabs_group}"] ||= {}
+    environment["tabs-#{@tabs_group}"][@tab] = content
+  end
+
+  private
+
+  def split_params(params)
+    params.split('#')
   end
 end
 
 PluginMetaLogger.instance.info { "Loaded #{JekyllBootstrap5Name::PLUGIN_NAME} v#{JekyllBootstrap5Tabs::VERSION} plugin." }
-Liquid::Template.register_tag('tabs', Jekyll::TabsBlock)
-Liquid::Template.register_tag('tab', Jekyll::TabBlock)
+Liquid::Template.register_tag('tabs', TabsBlock)
+Liquid::Template.register_tag('tab', TabBlock)
